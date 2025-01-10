a2a1b13fd933d140075c1ad8e38a074124d57126
30type <192151853+30type@users.noreply.github.com>
2025-01-10 13:10:46 +0000

    ,

--- keyboards/tominabox1/underscore33/rev2/keymaps/milk/keymap.c
+++ keyboards/tominabox1/underscore33/rev2/keymaps/milk/keymap.c
@@ -14,15 +14,34 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "features/achordion.h"
+#include "features/orbital_mouse.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (!process_achordion(keycode, record)) { return false; }
+
+  if (!process_orbital_mouse(keycode, record)) { return false; }
+  // Your macros ...
+
+
+  return true;
+}
+
+void matrix_scan_user(void) {
+  achordion_task();
+  orbital_mouse_task();
+
+}
 
 enum layers{
   _BASE,
   _SYM,
   _NAV,
+  _MSE,
   _EEP,
 };
 
-#define QK_MAGI QK_AREP
+#define LT_MSE MO(_MSE)
 #define LT_SPC LT(_SYM, KC_SPC)
 #define MT_LSFT OSM(MOD_LSFT)
 #define FN_NAV LT(_NAV, KC_ESC)
@@ -43,31 +62,38 @@ enum combo_events {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_33_split_space(
-    KC_Z,    KC_C,    KC_M,    KC_W,    KC_K,    KC_J,    KC_V,    KC_O,    KC_U,    KC_NO,
-    LT_CN,   LT_AS,   LT_GT,   LT_SY,   KC_F,    KC_H,    LT_SR,   LT_GA,   LT_AE,   LT_CI,
-    KC_B,    KC_NO,   KC_D,    KC_G,    KC_P,    KC_Q,    KC_L,    KC_X,    KC_NO,   CW_TOGG,
-                      KC_NO,   MT_LSFT, FN_NAV,  LT_SPC,  QK_MAGI,  KC_NO
+    KC_Z,    KC_C,    KC_M,    KC_W,    KC_K,    KC_J,    KC_V,    KC_O,    KC_U,    KC_NO  ,
+    LT_CN,   LT_AS,   LT_GT,   LT_SY,   KC_F,    KC_H,    LT_SR,   LT_GA,   LT_AE,   LT_CI  ,
+    KC_B,    KC_NO,   KC_D,    KC_G,    KC_P,    KC_Q,    KC_L,    KC_X,    KC_NO,   LT_MSE ,
+                      _______, MT_LSFT, FN_NAV , LT_SPC , LT_MSE , _______
   ),
 
   [_SYM] = LAYOUT_33_split_space(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_EQL,  KC_GRV,  KC_COLN, KC_TILD, KC_PLUS,
     KC_COMM, KC_DOT,  KC_SLSH, KC_ASTR, KC_CIRC, KC_LBRC, KC_LPRN, KC_LCBR, KC_DQUO, KC_MINS,
     KC_LT,   KC_GT,   KC_BSLS, KC_PIPE, KC_AMPR, KC_RBRC, KC_RPRN, KC_RCBR, KC_QUOT, KC_UNDS,
-                      KC_TRNS, KC_TRNS, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS
+                      _______, KC_TRNS, KC_SCLN, KC_TRNS, KC_TRNS, _______
   ),
 
   [_NAV] = LAYOUT_33_split_space(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL,
-    QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_TAB,  KC_NO,   KC_NO,
-                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    QK_BOOT, _______, _______, _______, _______, _______, KC_BSPC, KC_TAB,  _______, _______,
+                      _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, _______
+  ),
+
+  [_MSE] = LAYOUT_33_split_space(
+    _______, OM_DBLS, OM_SEL1, OM_SEL2, OM_SEL3, _______, _______, _______, _______, _______,
+    OM_SLOW, OM_L   , OM_U   , OM_R   , OM_W_U , _______, _______, _______, _______, _______,
+    OM_RELS, OM_HLDS, OM_D   , OM_BTNS, OM_W_D , _______, _______, _______, _______, _______,
+                      _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, _______
   ),
 
   [_EEP] = LAYOUT_33_split_space(
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                      _______, _______, _______, _______, _______, _______
   ),
 };
 
--- keyboards/tominabox1/underscore33/rev2/keymaps/milk/rules.mk
+++ keyboards/tominabox1/underscore33/rev2/keymaps/milk/rules.mk
@@ -1,3 +1,8 @@
 COMBO_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+SRC += features/achordion.c
+SRC += features/orbital_mouse.c
+
+MOUSE_ENABLE = yes
+MOUSEKEYS_ENABLE = yes
